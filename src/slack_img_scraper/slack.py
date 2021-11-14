60f1c7175da23ed16e523d2f565a55379a3603d3
Leo Hemsted <leohemsted@gmail.com>
2021-11-14 19:26:05 +0000

    only check new messages
    note that we still have to look at old messages to see if they have new thread responses

--- src/slack_img_scraper/slack.py
+++ src/slack_img_scraper/slack.py
@@ -13,58 +13,71 @@ DOWNLOAD_PATH = Path("output")
 
 
 class SlackImageDownloader:
-    def __init__(self, last_run: datetime = None):
+    def __init__(self):
         self.client = WebClient(token=os.environ["SLACK_TOKEN"])
-        self.since_ts = last_run.timestamp() if last_run else 0
-
-    def download_images(self):
-        channels = {
+        self.users = {x["id"]: x for x in self.client.users_list()["members"]}
+        self.channels = {
             channel["name"]: channel
             for channel in self.client.conversations_list()["channels"]
             # skip DMs etc
             if channel["is_channel"]
         }
 
+        with open(".last-run-ts.txt", "r") as last_run_f:
+            self.last_run_ts = last_run_f.read()
+
+    def download_images(self):
         for name_to_archive in CHANNELS_TO_ARCHIVE:
-            channel = channels[name_to_archive]
+            channel = self.channels[name_to_archive]
             self.get_images_for_channel(channel)
 
-    @staticmethod
-    def get_local_filename_for_file(channel, file, user):
+        with open(".last-run-ts.txt", "w") as last_run_f:
+            last_run_f.write(str(datetime.utcnow().timestamp()))
+
+    def get_local_filename_for_file(self, channel, file):
         """
         channel-date-username-time.filetype
         """
         dt = datetime.fromtimestamp(file["timestamp"])
+        user = self.users[file["user"]]
         username = user.get("real_name", user["name"]).lower().replace(" ", "-")
         return f"{channel['name']}-{dt.date().isoformat()}-{username}-{file['timestamp']}.{file['filetype']}"
 
     def get_image_files_for_history(self, channel, history, is_thread=False):
         for message in history["messages"]:
-            if "files" in message:
-                for file in message["files"]:
-                    if file["mimetype"].startswith("image"):
-                        print("found file", file["private_url"])
-                        yield file
-            # the first message in a thread is the thread starter, we don't want to recurse if we're inside
-            # the thread already so set a flag
+            # the first message in a thread is the thread starter, we don't want to
+            # recurse if we're inside the thread already so set a flag
             if not is_thread and "thread_ts" in message:
-                print("diving into a thread")
-                thread_history = self.client.conversations_replies(
-                    channel=channel["id"], ts=message["thread_ts"]
-                )
-                yield from self.get_image_files_for_history(
-                    channel, thread_history, is_thread=True
-                )
+                print(message["latest_reply"], self.last_run_ts)
+                if message["latest_reply"] >= self.last_run_ts:
+                    print("start thread")
+                    thread_history = self.client.conversations_replies(
+                        channel=channel["id"],
+                        ts=message["thread_ts"],
+                        oldest=self.last_run_ts,
+                    )
+                    yield from self.get_image_files_for_history(
+                        channel, thread_history, is_thread=True
+                    )
+                    print("finish thread")
+
+            # if the message is older than the last time we ran, then we don't need to
+            # re-download the files from it
+            if message["ts"] > self.last_run_ts:
+                if "files" in message:
+                    for file in message["files"]:
+                        if file["mimetype"].startswith("image"):
+                            print("found file", file["url_private"])
+                            yield file
         if history["has_more"]:
             print("more to come!")
 
     def get_images_for_channel(self, channel):
-        users = {x["id"]: x for x in self.client.users_list()["members"]}
+        # can't filter on old messages here in case there are new images inside threads
+        # that are replies to old messages
         history = self.client.conversations_history(channel=channel["id"])
         for file in self.get_image_files_for_history(channel, history):
-            local_filename = self.get_local_filename_for_file(
-                channel, file, users[file["user"]]
-            )
+            local_filename = self.get_local_filename_for_file(channel, file)
             resp = httpx.get(
                 file["url_private"],
                 headers={"Authorization": f"Bearer {self.client.token}"},
