a44286eb68f30dfa3f9c538426ec51fb1d9794bc
Leo Hemsted <leohemsted@gmail.com>
2022-03-09 19:40:02 +0000

    remove deprecated slack channel crawler

--- src/slack_img_scraper/cli.py
+++ src/slack_img_scraper/cli.py
@@ -3,9 +3,6 @@ from datetime import datetime
 
 import click
 
-from slack_img_scraper.slack_channel_crawler import (
-    SlackImageDownloader as ChannelDownloader,
-)
 from slack_img_scraper.slack_channels import SlackChannelJoiner
 from slack_img_scraper.slack_files import SlackImageDownloader as FileDownloader
 
@@ -13,14 +10,6 @@ from slack_img_scraper.slack_files import SlackImageDownloader as FileDownloader
 @click.group()
 def cli():
     pass
-
-
-@cli.command("by-channel")
-@click.option("-f", "--from-date", type=click.DateTime(), default=datetime.min)
-@click.option("-t", "--to-date", type=click.DateTime(), default=datetime.utcnow())
-def download_historical_images_by_channel(from_date, to_date):
-    downloader = ChannelDownloader()
-    asyncio.run(downloader.download_images())
 
 
 @cli.command("by-file")
